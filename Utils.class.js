86dad07d198628188c67427bda9b7a21c33b4d72
nguyenHoangDung0504 <sdjklo2021@gmail.com>
2025-02-03 17:57:32 +0700

    3/2/2025 - 5:57 PM - Activated cache

--- Utils.class.js
+++ Utils.class.js
@@ -161,13 +161,13 @@ export default class Utils {
         }
 }
 
-// if ('serviceWorker' in navigator && !navigator.serviceWorker.controller) {
-//     console.log('Registering Service Worker...');
-//     navigator.serviceWorker.register('/cache.js')
-//         .then((registration) => {
-//             console.log('Service Worker registered with scope:', registration.scope);
-//         })
-//         .catch((error) => {
-//             console.error('Service Worker registration failed:', error);
-//         });
-// }
+if ('serviceWorker' in navigator && !navigator.serviceWorker.controller) {
+    console.log('Registering Service Worker...');
+    navigator.serviceWorker.register('/cache.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+        });
+}
